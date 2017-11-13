5a99971ad38922f7e2245ad1286d62ebcf020d2f
邓春亚 <dengchunya@calfdata.com>
2017-11-13 14:33:18 +0800

    init

---
+++ src/main/java/com/cloud/disk/DiskApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.disk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiskApplication.class, args);
+	}
+}
